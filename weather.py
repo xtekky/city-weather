bf07731a1a1a7e3b99c0a4763659eb96675ecddf
The Trading Guy <98614666+xtekky@users.noreply.github.com>
2022-03-05 22:54:33 +0100

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,65 @@
+from selenium.webdriver.common.keys import Keys as k
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("https://google.com/")
+wait = WebDriverWait(driver, 20)
+
+def process():
+    global temperature
+    global precipitation
+    global wind
+    global humidity
+    global time
+    global forecast
+
+    #accepting google cookies and selecting language
+    wait.until(EC.visibility_of_element_located((By.CLASS_NAME,'V5OCtd'))).click()
+    if language == 'en':
+        wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="tbTubd"]/div/li[13]'))).click()
+    if language == 'fr':
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="tbTubd"]/div/li[18]'))).click()
+    if language == 'de':
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="tbTubd"]/div/li[9]'))).click()
+
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="L2AGLb"]/div'))).click()
+
+    #searching for weather
+    wait.until(EC.visibility_of_element_located((By.NAME, 'q'))).send_keys(city, ' weather'+k.RETURN)
+
+    #scraping weather data and assigning to variables
+    temperature = wait.until(EC.visibility_of_element_located((By.ID, 'wob_tm'))).text
+    wind = wait.until(EC.visibility_of_element_located((By.ID, 'wob_ws'))).text
+    precipitation = wait.until(EC.visibility_of_element_located((By.ID, 'wob_pp'))).text
+    humidity = wait.until(EC.visibility_of_element_located((By.ID, 'wob_hm'))).text
+    time = wait.until(EC.visibility_of_element_located((By.ID, 'wob_dts'))).text
+    forecast = wait.until(EC.visibility_of_element_located((By.ID, 'wob_dc'))).text
+
+
+def info():
+
+    #printing weather info
+    print('Date: ', time)
+    print('Forecast: ', forecast)
+    print('Temperature: ',temperature,'°C')
+    print('Wind: ',wind)
+    print('Precipitation: ', precipitation)
+    print('Humidity: ', humidity)
+
+#city input
+city = input('City: ')
+language = input('Language [en,fr,de]: ')
+if language != 'fr':
+    if language != 'de':
+         if language !='en':
+            print('Language not supported')
+            exit()
+process()
+info()
+
+#leave driver
+driver.quit()
